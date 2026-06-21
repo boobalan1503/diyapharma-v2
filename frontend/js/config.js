81a35172bfcaf5e0712c9d9a606e77af11213675
Boobalan <boobalan1503>
2026-06-21 22:38:18 +0530

    client id updated for new gmail

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,5 +3,5 @@ const CONFIG = {
   API_BASE_URL: 'https://diyapharma-v2.vercel.app/api',
   // Google OAuth Client ID — replace with your own from Google Cloud Console
   // https://console.cloud.google.com/apis/credentials
-  GOOGLE_CLIENT_ID: '294358984739-svbd9501ajr1dj3ancrtuouffhj79sj9.apps.googleusercontent.com'
+  GOOGLE_CLIENT_ID: '222773803915-k2p1cih6n43h2q3qg7ok8s3480j7crsf.apps.googleusercontent.com'
 };
